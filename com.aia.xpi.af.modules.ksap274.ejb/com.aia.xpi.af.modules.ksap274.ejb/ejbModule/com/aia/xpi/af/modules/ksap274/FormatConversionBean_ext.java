d1680380deb3acc66347ada17180780d07c9c89e
Chua Yuan Wei <alexchua2004@gmail.com>
2019-06-26 14:04:00 +0800

    initial

---
+++ com.aia.xpi.af.modules.ksap274.ejb/com.aia.xpi.af.modules.ksap274.ejb/ejbModule/com/aia/xpi/af/modules/ksap274/FormatConversionBean_ext.java
@@ -0,0 +1,23 @@
+package com.aia.xpi.af.modules.ksap274;
+
+import com.equalize.xpi.af.modules.util.AbstractModule;
+import com.equalize.xpi.af.modules.util.AbstractModuleConverter;
+import com.equalize.xpi.af.modules.util.ConverterFactory;
+import com.sap.aii.af.lib.mp.module.ModuleException;
+import com.sap.engine.interfaces.messaging.api.auditlog.AuditLogStatus;
+
+public class FormatConversionBean_ext extends AbstractModule {
+	@Override
+	protected void processModule() throws ModuleException {
+		if (this.debug)
+			this.audit.addLog(AuditLogStatus.SUCCESS, "DEBUG: System default encoding:- " + System.getProperty("file.encoding"));
+		ConverterFactory factory = ConverterFactory.newInstance();
+		AbstractModuleConverter converter = factory.newConverter(this.msg, this.param, this.audit, this.dyncfg, this.debug);
+		converter.retrieveModuleParameters();
+		converter.parseInput();
+		byte[] outputBytes = converter.generateOutput();
+
+		// Update payload content
+		changePayloadContent(outputBytes);
+	}
+}
